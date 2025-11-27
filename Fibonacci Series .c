8e0e702e7a497ed950b7de89f7858fac3a6381f1
sriram-711 <145254817+sriram-711@users.noreply.github.com>
2025-11-27 18:12:05 +0530

    Update Fibonacci Series .c

--- Fibonacci Series .c
+++ Fibonacci Series .c
@@ -22,3 +22,31 @@ for(i=0;i<n;i++)
 }
 return 0;
 }
+///////////////////////////////////////
+#include<stdio.h>
+int main()
+{
+    int n,a=0,b=1,i,sum=0;
+    scanf("%d",&n);
+    if(n==0)
+    {
+        printf("%d",a);
+        return 0;
+    }
+    else if(n==1)
+    {
+        printf("%d",a);
+        return 0;
+    }
+    else{
+        printf("%d",a);
+        for(i=0;i<n;i++)
+        {
+            sum=a+b;
+            b=a;
+            a=sum;
+            printf(" %d",sum);
+        }
+    }
+    return 0;
+}
