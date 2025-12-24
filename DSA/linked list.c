0a808a2fdc2385df5e3eb647bf0b34d2a5305363
sriram-711 <145254817+sriram-711@users.noreply.github.com>
2025-12-24 18:32:30 +0530

    Update linked list.c

--- DSA/linked list.c
+++ DSA/linked list.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+struct node {
+    int data;
+    struct node *link;
+};
+
+int main() {
+    int n, i;
+    struct node *newnode, *temp, *head;
+
+    head = NULL;
+
+    printf("Enter size of linked list: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <=n; i++) {
+        newnode = malloc(sizeof(struct node));
+        printf("Enter data for node %d: ", i);
+        scanf("%d", &newnode->data);
+        newnode->link = NULL;
+
+        if (head == NULL) {
+            head = newnode;
+            temp = head;
+        } else {
+            temp->link = newnode;
+            temp = newnode;
+        }
+    }
+
+    /* Print linked list */
+    temp = head;
+    printf("Linked List: ");
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->link;
+    }
+    printf("NULL\n");
+
+    return 0;
+}.    
